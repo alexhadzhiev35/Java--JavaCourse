761db46eb7beff87fe00eef624f3c7bafb45748f
alexhadzhiev35 <alexhadzhiev35@gmail.com>
2023-11-05 10:45:27 +0200

    added Coding Exercises

--- OOP Part 1/CodingExercises36-37/src/Main.java
+++ OOP Part 1/CodingExercises36-37/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        // 36----------------------------------
 //        Circle circle = new Circle(3.75);
 //        System.out.println("circle.radius= " + circle.getRadius());
 //        System.out.println("circle.area= " + circle.getArea());
@@ -9,5 +11,19 @@ public class Main {
 //        System.out.println("cylinder.height= " + cylinder.getHeight());
 //        System.out.println("cylinder.area= " + cylinder.getArea());
 //        System.out.println("cylinder.volume= " + cylinder.getVolume());
+
+        // 37----------------------------------
+
+        Rectangle rectangle = new Rectangle(5, 10);
+        System.out.println("rectangle.width= " + rectangle.getWidth());
+        System.out.println("rectangle.length= " + rectangle.getLength());
+        System.out.println("rectangle.area= " + rectangle.getArea());
+        Cuboid cuboid = new Cuboid(5,10,5);
+        System.out.println("cuboid.width= " + cuboid.getWidth());
+        System.out.println("cuboid.length= " + cuboid.getLength());
+        System.out.println("cuboid.area= " + cuboid.getArea());
+        System.out.println("cuboid.height= " + cuboid.getHeight());
+        System.out.println("cuboid.volume= " + cuboid.getVolume());
+
     }
 }
